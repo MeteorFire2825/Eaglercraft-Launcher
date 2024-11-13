74593356914e4089b874fe09cf06d2ea65b8e752
MeteorFire2825 <islam2825.202@gmail.com>
2024-11-13 18:09:14 +0000

    update

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 // firebase-config.js
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    apiKey: "AIzaSyDatVTrM54KF8EkntkDXsjYXbxkoVOrYC4
+",
+    authDomain: "eaglercraft-24c5b.firebaseapp.com",
+    projectId: "eaglercraft-24c5b",
+    storageBucket: "eaglercraft-24c5b.appspot.com",
     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    appId: "1:793669838534:web:b70517e880777c37a019c7"
 };
 
 // Initialize Firebase
